706f16702144425f2a699944a9c5c13426eb28ff
santiago-lopez-aquino <68907586+santiago-lopez-aquino@users.noreply.github.com>
2020-08-08 19:47:26 -0300

    Consola
    Mostrar listado en la estufa

--- js/products.js
+++ js/products.js
@@ -1,6 +1,13 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+//document.addEventListener("DOMContentLoaded", function(e) {});
 
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            console.log(resultObj);
+            //sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+        }
+    });
 });
