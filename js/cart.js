ac9023630d4d2cd30ee178f4f868e0be0cae52e5
santiago-lopez-aquino <68907586+santiago-lopez-aquino@users.noreply.github.com>
2020-11-03 09:56:01 -0300

    Actualización del carrito
    Modificación del carrito

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,49 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var cantidad = [];
+var variable = [];
+var cartProducts = [];
+var count = [];
+
+function showCart() {
+    let htmlContentToAppend = "";
+    for (let i = 0; i < cartProducts.length; i++) {
+        htmlContentToAppend += `
+        <div class="row">
+            <div class"col-md-2">
+                <img src="` + cartProducts[i].src + `" alt="..." class="d-block w-100">
+            </div>
+            <p class="col-md-4">` + cartProducts[i].name + `</p>
+            <p class="col-md-2">` + cartProducts[i].currency + `  ` + cartProducts[i].unitCost + `</p>
+            <div class="col-md-2"><input class="form-control" type="number" onclick="updatePrice();" placeholder="` + cartProducts[i].count + `" id="count` + i + `" value="` + count[i] + `"></div>
+            <p class="col">` + cartProducts[i].currency + ` <spam id=subtotal>` + cartProducts[i].unitCost * cartProducts[i].count + `</spam></p>
+        </div>       
+        <hr>
+        `
+    }
+    document.getElementById("cart").innerHTML = htmlContentToAppend;
+}
+
+function updatePrice() {
+    for (let i = 0; i < cartProducts.length; i++) {
+        cantidad = document.getElementById("count" + i).value;
+        variable = cantidad * cartProducts[i].unitCost;
+
+    }
+    document.getElementById("subtotal").innerHTML = variable;
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(CART_INFO_URL).then(function(resultObj) {
+        if (resultObj.status === 'ok') {
+            console.log(resultObj.data.articles)
+            cartProducts = resultObj.data.articles;
+        }
+
+        showCart();
+
+
+    });
 
 });
